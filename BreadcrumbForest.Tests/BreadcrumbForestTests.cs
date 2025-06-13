0113f89b556c5b769c4728841a15b0958e0f6fad
Cj-bc <cj.bc-sd@outlook.jp>
2025-06-13 19:11:01 +0900

    feat(test): Add test for BreadcrumbForest.Next

--- BreadcrumbForest.Tests/BreadcrumbForestTests.cs
+++ BreadcrumbForest.Tests/BreadcrumbForestTests.cs
@@ -173,4 +173,164 @@ public class BreadcrumbForestTests
         Assert.That(forest.Current, Is.Not.Null);
         Assert.That(forest.Current, Is.EqualTo("OnlyItem"));
     }
+
+    [Test]
+    public void Next_OnSingleLeafNode_ReturnsFalse()
+    {
+        var forest = BreadcrumbForest<string>.FromItems(item =>
+            item("OnlyLeaf")
+        );
+
+        bool result = forest.Next();
+
+        Assert.That(result, Is.False);
+        Assert.That(forest.Current, Is.EqualTo("OnlyLeaf"));
+    }
+
+    [Test]
+    public void Next_OnParentWithChildren_MovesToFirstChild()
+    {
+        var forest = BreadcrumbForest<string>.FromItems(item =>
+            item("Parent").WithChildren(
+                item("Child1"),
+                item("Child2")
+            )
+        );
+
+        forest.SetCurrent("Parent");
+        bool result = forest.Next();
+
+        Assert.That(result, Is.True);
+        Assert.That(forest.Current, Is.EqualTo("Child1"));
+    }
+
+    [Test]
+    public void Next_OnFirstChildWithSiblings_MovesToNextSibling()
+    {
+        var forest = BreadcrumbForest<string>.FromItems(item =>
+            item("Parent").WithChildren(
+                item("Child1"),
+                item("Child2"),
+                item("Child3")
+            )
+        );
+
+        forest.SetCurrent("Child1");
+        bool result = forest.Next();
+
+        Assert.That(result, Is.True);
+        Assert.That(forest.Current, Is.EqualTo("Child2"));
+    }
+
+    [Test]
+    public void Next_OnLastSiblingLeaf_MovesToParentNextSibling()
+    {
+        var forest = BreadcrumbForest<string>.FromItems(item =>
+            item("Root").WithChildren(
+                item("Parent1").WithChildren(
+                    item("Child1"),
+                    item("Child2")
+                ),
+                item("Parent2")
+            )
+        );
+
+        forest.SetCurrent("Child2");
+        bool result = forest.Next();
+
+        Assert.That(result, Is.True);
+        Assert.That(forest.Current, Is.EqualTo("Parent2"));
+    }
+
+    [Test]
+    public void Next_CompleteTreeTraversal_VisitsAllNodesInOrder()
+    {
+        var forest = BreadcrumbForest<string>.FromItems(item =>
+            item("Root").WithChildren(
+                item("A").WithChildren(
+                    item("A1"),
+                    item("A2")
+                ),
+                item("B").WithChildren(
+                    item("B1")
+                ),
+                item("C")
+            )
+        );
+
+        List<string> visitedNodes = [];
+        
+        while (forest.Next())
+        {
+            visitedNodes.Add(forest.Current);
+        }
+
+        Assert.That(visitedNodes, Is.EqualTo(new[] { "A1", "A2", "B1", "C" }));
+    }
+
+    [Test]
+    public void Next_AtLastNodeInTree_ReturnsFalse()
+    {
+        var forest = BreadcrumbForest<string>.FromItems(item =>
+            item("Root").WithChildren(
+                item("Child1"),
+                item("Child2")
+            )
+        );
+
+        forest.SetCurrent("Child2");
+        bool result = forest.Next();
+
+        Assert.That(result, Is.False);
+        Assert.That(forest.Current, Is.EqualTo("Child2"));
+    }
+
+    [Test]
+    public void Next_OnNestedStructure_TraversesDepthFirst()
+    {
+        var forest = BreadcrumbForest<string>.FromItems(item =>
+            item("1").WithChildren(
+                item("1.1").WithChildren(
+                    item("1.1.1"),
+                    item("1.1.2")
+                ),
+                item("1.2")
+            )
+        );
+
+        List<string> traversalOrder = [];
+        
+        while (forest.Next())
+        {
+            traversalOrder.Add(forest.Current);
+        }
+
+        Assert.That(traversalOrder, Is.EqualTo(new[] { "1.1.1", "1.1.2", "1.2" }));
+    }
+
+    [Test]
+    public void Next_WithMultipleSiblingBranches_TraversesCorrectly()
+    {
+        var forest = BreadcrumbForest<string>.FromItems(item =>
+            item("Root").WithChildren(
+                item("Branch1").WithChildren(
+                    item("Leaf1")
+                ),
+                item("Branch2").WithChildren(
+                    item("Leaf2"),
+                    item("Leaf3")
+                ),
+                item("Branch3")
+            )
+        );
+
+        List<string> traversalOrder = [];
+        
+        while (forest.Next())
+        {
+            traversalOrder.Add(forest.Current);
+        }
+
+        Assert.That(traversalOrder, Is.EqualTo(new[] { "Leaf1", "Leaf2", "Leaf3", "Branch3" }));
+    }
 }
